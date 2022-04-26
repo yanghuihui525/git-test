bb86bd1b5274ea653e861cf4c2999ab89ada31c2
yanghuihui <2781110627@qq.com>
2022-04-26 18:42:26 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,7 +5,8 @@ package com.atguigu.git;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello,git");
-        System.out.println("hello,git22");
+        System.out.println("hello,git1");
+        System.out.println("hello,git2");
+        System.out.println("hello,git3");
     }
 }
