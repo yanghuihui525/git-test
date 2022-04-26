882642e2dd6e7b63d534ae45965d01ccb231ac8a
yanghuihui <2781110627@qq.com>
2022-04-26 19:04:53 +0800

    master6 commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,9 +9,10 @@ public class GitTest {
         System.out.println("hello,git1");
         System.out.println("hello,git2");
         System.out.println("hello,git3");
-
-        System.out.println("hello,git");
         System.out.println("hello,git4");
+        System.out.println("hello,git5");
+        System.out.println("hello,git6");
+
 
     }
 }
